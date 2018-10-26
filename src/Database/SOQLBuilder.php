c3f6afdc216c1378228b87526066627c28bb4761
Rob Lester <rlester@liveperson.com>
2018-10-25 22:10:23 -0400

    no message

--- src/Database/SOQLBuilder.php
+++ src/Database/SOQLBuilder.php
@@ -72,19 +72,20 @@ class SOQLBuilder extends Builder
 		    $layouts = \Forrest::sobjects($table . '/' . config('eloquent_sf.layout') . '/');
 		    $fields = array_pluck($layouts["fieldItems"], 'layoutComponents.0');
 		    $columns = ['Id'];
-		    foreach ($fields as $field) {
-			    if ($field['details']['updateable'] == true) {
-				    $columns[] = $field['details']['name'];
-			    }
-			    if (isset($field['components'])) {
-				    foreach ($field['components'] as $component) {
-					    $columns[] = $component['details']['name'];
-				    }
-			    }
-		    }
-		    
+		    $this->getDetailNames($fields, $columns);
 	    }
 	    return $columns;
+    }
+    
+    private function getDetailNames($fields, &$columns) {
+	    foreach ($fields as $field) {
+		    if ($field['details']['updateable'] == true) {
+			    $columns[] = $field['details']['name'];
+		    }
+		    if (isset($field['components'])) {
+			    $this->getDetailNames($field['components'], $columns);
+		    }
+		}
     }
 	
 	
--- tests/EloquentSalesForceTest.php
+++ tests/EloquentSalesForceTest.php
@@ -32,6 +32,60 @@ class EloquentSalesForceTest extends TestCase
 		parent::setUp();
 		
 		config([
+			'forrest' => [
+				/*
+			     * These are optional authentication parameters that can be specified for the WebServer flow.
+			     * https://help.salesforce.com/apex/HTViewHelpDoc?id=remoteaccess_oauth_web_server_flow.htm&language=en_US
+			     */
+			    'parameters'     => [
+			        'display'   => '',
+			        'immediate' => false,
+			        'state'     => '',
+			        'scope'     => '',
+			        'prompt'    => '',
+			    ],
+			
+			    /*
+			     * Default settings for resource requests.
+			     * Format can be 'json', 'xml' or 'none'
+			     * Compression can be set to 'gzip' or 'deflate'
+			     */
+			    'defaults'       => [
+			        'method'          => 'get',
+			        'format'          => 'json',
+			        'compression'     => false,
+			        'compressionType' => 'gzip',
+			    ],
+			
+			    /*
+			     * Where do you want to store access tokens fetched from Salesforce
+			     */
+			    'storage'        => [
+			        'type'          => 'session', // 'session' or 'cache' are the two options
+			        'path'          => 'forrest_', // unique storage path to avoid collisions
+			        'expire_in'     => 20, // number of minutes to expire cache/session
+			        'store_forever' => false, // never expire cache/session
+			    ],
+			
+			    /*
+			     * If you'd like to specify an API version manually it can be done here.
+			     * Format looks like '32.0'
+			     */
+			    'version'        => '',
+			
+			    /*
+			     * Optional (and not recommended) if you need to override the instance_url returned from Saleforce
+			     */
+			    'instanceURL'    => '',
+			
+			    /*
+			     * Language
+			     */
+			    'language'       => 'en_US',
+			]
+		]);
+		
+		config([
 			'app.key' => 'base64:WRAf0EDpFqwpbS829xKy2MGEkcJxIEmMrwFIZbGxIqE=',
 			'cache.stores.file.path' => __DIR__,
 			'cache.default' => 'file',
@@ -47,21 +101,9 @@ class EloquentSalesForceTest extends TestCase
 		        'username'       => getenv('USERNAME'),
 		        // Security token might need to be ammended to password unless IP Address is whitelisted
 		        'password'       => getenv('PASSWORD')
-			],
-			'forrest.defaults'    => [
-		        'method'          => 'get',
-		        'format'          => 'json',
-		        'compression'     => false,
-		        'compressionType' => 'gzip',
-		    ],
+			]
 		]);
-		
-		/*config([
-			'forrest.credentials' => config('database.connections.soql')
-		]);*/
-		
-		//dd(config('forrest'));
-		
+				
 	}
 	
 	/**
