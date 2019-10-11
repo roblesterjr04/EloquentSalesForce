84be8cde6acd4b5357255960b61240c4ed8e7bf2
Robert Lester <rlester@liveperson.com>
2019-10-11 16:35:29 -0400

    ..

--- src/SalesForceObject.php
+++ src/SalesForceObject.php
@@ -4,5 +4,9 @@ namespace Lester\EloquentSalesForce;
 
 class SalesForceObject extends Model
 {
-
+    public function __construct(Array $attributes)
+    {
+        $this->table = $attributes['attributes']['type'];
+        parent::__construct($attributes);
+    }
 }
